572b4d0f11cbe6ad065821f6bf18bec46cc36999
Silas Nordgren <nordgrensilas@gmail.com>
2019-05-17 14:29:57 +0200

    Update currying test to agree with new type checking

--- fjord-compiler/test/codegen/functions/Curry.js
+++ fjord-compiler/test/codegen/functions/Curry.js
@@ -1,22 +1,16 @@
 // module test.codegen.functions.Curry
 
-var first = function(x, y, z) {
-  return x;
+var add = function(x, y) {
+  return 0;
 };
-exports.first = first;
+exports.add = add;
 
-var alwaysOne = function(_0, _1) {
-  return (first(1, _0, _1));
+var useFOn3 = function(f) {
+  return (f(3));
 };
-exports.alwaysOne = alwaysOne;
+exports.useFOn3 = useFOn3;
 
-var alwaysTwo = function(x, _0) {
-  return (first(2, x, _0));
-};
-exports.alwaysTwo = alwaysTwo;
-
-var one = (alwaysOne(2, 3));
-exports.one = one;
-
-var two = (alwaysTwo(3, 4));
-exports.two = two;
+var eight = (useFOn3(function(_0) {
+  return (add(5, _0));
+}));
+exports.eight = eight;
